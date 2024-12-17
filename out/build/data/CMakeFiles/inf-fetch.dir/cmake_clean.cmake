960d658fec0e5e22e7213d524099c1678951017b
The-Infinitys <theinfinitysinfinity@gmail.com>
2024-12-17 06:36:01 +0000

    COMPLETE!

---
+++ out/build/data/CMakeFiles/inf-fetch.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/inf-fetch.dir/main.cpp.o"
+  "inf-fetch"
+  "inf-fetch.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/inf-fetch.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
